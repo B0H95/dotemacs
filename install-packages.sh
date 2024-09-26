155b17cf5519ad2d64dd48e2ca743cacd6bdce54
B0H <10817629+B0H95@users.noreply.github.com>
2024-09-26 14:54:01 +0200

    adding other configs

---
+++ install-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dnf install \
+    i3 i3status i3lock dex-autostart xss-lock network-manager-applet pasystray \
+    emacs emacs-gtk+x11 \
+    gcc libgccjit clang clang-tools-extra \
+    make cmake \
+    git \
+    python3 python3-pip \
+    zip unzip \
+    dmenu \
+    gnuplot \
+    texlive-scheme-full \
+    raylib raylib-devel \
+    SDL2 SDL2-devel SDL2_image SDL2_image-devel \
+    SDL2_mixer SDL2_mixer-devel SDL2_net SDL2_net-devel \
+    SDL2_sound SDL2_sound-devel SDL2_ttf SDL2_ttf-devel \
+    SDL2_gfx SDL2_gfx-devel \
+    unrar
